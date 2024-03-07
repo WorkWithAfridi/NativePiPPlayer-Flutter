98f176b68509c102b16926985d307c9cde012e2e
khondakar.afridi <afridi.khondakar@gmail.com>
2024-03-07 17:47:27 +0600

    Firebase storage version downgraded due to sdk issue

--- lib/pip_view.dart
+++ lib/pip_view.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:floating/floating.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gap/gap.dart';
@@ -13,8 +14,9 @@ class FlutterPipView extends StatefulWidget {
   State<FlutterPipView> createState() => _FlutterPipViewState();
 }
 
-class _FlutterPipViewState extends State<FlutterPipView> {
+class _FlutterPipViewState extends State<FlutterPipView> with WidgetsBindingObserver {
   late VideoPlayerController _controller;
+  final floating = Floating();
 
   @override
   void initState() {
@@ -27,89 +29,113 @@ class _FlutterPipViewState extends State<FlutterPipView> {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
       });
+
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    floating.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (Platform.isAndroid) {
+      if (state == AppLifecycleState.inactive) {
+        floating.enable();
+        _controller.play();
+      }
+    }
+    super.didChangeAppLifecycleState(state);
+  }
+
+  Future enablePip() async {
+    final status = await floating.enable();
+    print("Enabled pip: $status");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _controller.value.isPlaying ? _controller.pause() : _controller.play();
-          });
-        },
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-        ),
-      ),
       body: SizedBox(
         width: Get.width,
         height: Get.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _controller.value.isInitialized
-                ? Column(
-                    children: [
-                      AspectRatio(
-                        aspectRatio: _controller.value.aspectRatio,
-                        child: VideoPlayer(
-                          _controller,
-                        ),
-                      ),
-                      SizedBox(
-                        width: Get.width,
-                        child: VideoProgressIndicator(
-                          _controller,
-                          allowScrubbing: true,
-                          colors: const VideoProgressColors(
-                            playedColor: Colors.pink,
-                            bufferedColor: Colors.grey,
-                            backgroundColor: Colors.transparent,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _controller.value.isInitialized
+                  ? Column(
+                      children: [
+                        AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: VideoPlayer(
+                            _controller,
                           ),
                         ),
-                      )
-                    ],
-                  )
-                : Container(),
-            const Gap(12),
-            ElevatedButton(
-              onPressed: () {
-                if (Platform.isIOS) {
-                  print("Current duration: ${_controller.value.position}");
-                  _controller.pause();
-                  Get.to(
-                    () => SwiftUIPipView(
-                      mode: "pip",
-                      duration: _controller.value.position.inMilliseconds,
-                    ),
-                  );
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                    content: Text("Native swift code can only be run on iOS devices."),
-                  ));
-                }
-              },
-              child: const Text('START-PIP-SWIFTUI'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (Platform.isIOS) {
-                  Get.to(
-                    () => SwiftUIPipView(
-                      mode: "background-play",
-                      duration: const Duration(seconds: 0).inMilliseconds,
-                    ),
-                  );
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                    content: Text("Native swift code can only be run on iOS devices."),
-                  ));
-                }
-              },
-              child: const Text('START-BG-PLAY'),
-            )
-          ],
+                        SizedBox(
+                          width: Get.width,
+                          child: VideoProgressIndicator(
+                            _controller,
+                            allowScrubbing: true,
+                            colors: const VideoProgressColors(
+                              playedColor: Colors.pink,
+                              bufferedColor: Colors.grey,
+                              backgroundColor: Colors.transparent,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  : Container(),
+              const Gap(12),
+              ElevatedButton(
+                onPressed: () {
+                  if (Platform.isIOS) {
+                    print("Current duration: ${_controller.value.position}");
+                    _controller.pause();
+                    Get.to(
+                      () => SwiftUIPipView(
+                        mode: "pip",
+                        duration: _controller.value.position.inMilliseconds,
+                      ),
+                    );
+                  } else {
+                    enablePip();
+                  }
+                },
+                child: const Text('START-PIP'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (Platform.isIOS) {
+                    Get.to(
+                      () => SwiftUIPipView(
+                        mode: "background-play",
+                        duration: const Duration(seconds: 0).inMilliseconds,
+                      ),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text("Native swift code can only be run on iOS devices."),
+                    ));
+                  }
+                },
+                child: const Text('START-BG-PLAY'),
+              ),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _controller.value.isPlaying ? _controller.pause() : _controller.play();
+                    });
+                  },
+                  icon: Icon(
+                    _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                  ))
+            ],
+          ),
         ),
       ),
     );
